8631dcddea13707dd36c4af4459e6c308a64267a
Carlos Martinez <74222223+CarlosMartinez890@users.noreply.github.com>
2025-01-24 21:39:41 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+// -----------------------------
+// Variable Declarations
+// -----------------------------
+
+
+
+// -----------------------------
+// Helper Functions
+// -----------------------------
+
+function flashResult() {
+
+}
+
+function computerPlay() {
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+// -----------------------------
+// Chat Functionality
+// -----------------------------
+
+function addChatMessage(message) {
+
+}
+
+// -----------------------------
+// Event Listeners
+// -----------------------------
+
+buttons.forEach((button) => {
+  
+});
